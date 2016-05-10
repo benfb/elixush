cc3ba71941dd02cf6d5a9dc9a7cedf6bd56913e6
Ben Bailey <bennettbailey@gmail.com>
2016-05-10 14:18:54 -0500

    remove extraneous dependencies

--- mix.exs
+++ mix.exs
@@ -31,8 +31,6 @@ defmodule Elixush.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:earmark, "~> 0.1", only: :dev},
-      {:ex_doc, "~> 0.11", only: :dev},
       {:zipper, "~> 0.2.0"}
     ]
   end
