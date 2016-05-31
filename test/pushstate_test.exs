b25f407077db1875218804449ff372df93c862e6
Ben Bailey <bennettbailey@gmail.com>
2016-05-31 11:03:25 -0500

    add pushstate tests

---
+++ test/pushstate_test.exs
@@ -0,0 +1,20 @@
+defmodule Elixush.PushStateTest do
+  use ExUnit.Case, async: true
+  import Elixush.PushState
+  # alias Elixush.Server
+
+  test "the result of make_push_state is empty" do
+    state = make_push_state
+    assert state |> Map.get(:boolean) |> List.first == nil
+    assert state |> Map.get(:integer) |> List.first == nil
+    assert state |> Map.get(:string) |> List.first == nil
+  end
+
+  test "push_item pushes an item successfully" do
+    assert push_item(true, :boolean, make_push_state)
+           |> Map.get(:boolean)
+           |> List.first == true
+  end
+
+
+end
